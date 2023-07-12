3b2e80327882780ce4cf94e9dc69c273016a9846
Vivek Kumar <vivek.kumar@weareflexday.com>
2023-07-12 09:32:25 +0530

    add test reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,19 @@ module.exports = defineConfig({
   env: {
     APP_URL: 'http://localhost:3000', // should go in .env file
   },
+  video: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
